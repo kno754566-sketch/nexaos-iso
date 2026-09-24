8c578c833bcbbc63682bd335276905c4b6052f8a
kno754566-sketch <320016265+kno754566-sketch@users.noreply.github.com>
2026-09-24 20:45:48 +0900

    Windows 11風の見た目

---
+++ config/includes.chroot/usr/share/nexaos/panel.js
@@ -0,0 +1,17 @@
+function spacer(p) { var s = p.addWidget("org.kde.plasma.panelspacer"); s.currentConfigGroup = ["General"]; s.writeConfig("expanding", "true"); }
+var p = panels()[0];
+if (p) {
+  var ids = p.widgetIds;
+  for (var i = 0; i < ids.length; i++) { p.widgetById(ids[i]).remove(); }
+  p.location = "bottom";
+  p.height = 46;
+  spacer(p);
+  p.addWidget("org.kde.plasma.kickoff");
+  var t = p.addWidget("org.kde.plasma.icontasks");
+  t.currentConfigGroup = ["General"];
+  t.writeConfig("fill", "false");
+  t.writeConfig("launchers", "applications:nexaweb.desktop,applications:org.kde.dolphin.desktop,applications:org.kde.konsole.desktop,applications:org.kde.plasma-systemmonitor.desktop");
+  spacer(p);
+  p.addWidget("org.kde.plasma.systemtray");
+  p.addWidget("org.kde.plasma.digitalclock");
+}
